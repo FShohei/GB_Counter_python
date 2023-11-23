91a888dae206b5f97a07a4d1e37aec563e762f86
FShohei <takumi0mhp@gmail.com>
2023-11-23 15:47:02 +0900

    add function to read data

--- GB_Counter.py
+++ GB_Counter.py
@@ -1,30 +1,44 @@
 import tkinter as tk
+import csv
 
+"""
+count_1 = 青箱
+count_2 = ヒヒイロカネ
+"""
 total_count_1 = 0
 total_count_2 = 0
 count_1 = 0
 count_2 = 0
 
+with open('temp.csv', mode = 'r', encoding = 'utf-8') as input_file:
+    init_datas = csv.reader(input_file)
+    next(init_datas)
+
+    for init_data in init_datas:
+        total_count_1 = int(init_data[1])
+        total_count_1 = int(init_data[2])
+
+
 def increment_count_1():
     global count_1
     count_1 += 1
-    count_label_1.config(text = "Count 1: " + str(count_1))
+    count_label_1.config(text = "青箱: " + str(count_1))
 
 def increment_count_2():
     global count_2
     count_2 += 1
-    count_label_2.config(text = "Count 2: " + str(count_2))
+    count_label_2.config(text = "ヒヒ: " + str(count_2))
 
 # Tkinterウィンドウの作成
 root = tk.Tk()
-root.title("カウンターアプリ")
+root.title("アーカーシャ カウント")
 
 # Count 1
 frame_1 = tk.Frame(root)
 frame_1.pack()
 
 # カウント表示用のラベル
-count_label_1 = tk.Label(frame_1, text = "Count 1: " + str(count_1))
+count_label_1 = tk.Label(frame_1, text = "青箱: " + str(count_1))
 count_label_1.pack(side = tk.LEFT)
 
 # カウント増加ボタン
@@ -36,7 +50,7 @@ increment_button_1.pack(side = tk.LEFT)
 frame_2 = tk.Frame(root)
 frame_2.pack()
 
-count_label_2 = tk.Label(frame_2, text = "Count 2: " + str(count_2))
+count_label_2 = tk.Label(frame_2, text = "ヒヒ: " + str(count_2))
 count_label_2.pack(side = tk.LEFT)
 
 increment_button_2 = tk.Button(frame_2, text="増加", command=increment_count_2)
